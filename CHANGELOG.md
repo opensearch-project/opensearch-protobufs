bb55b6185d21376a1f3f842c31b52e0d2bb3dab6
lucy66hw <fridalu66@gmail.com>
2025-04-09 14:09:14 -0700

    Preprocessing oneof by aggregating array of item and single item (#76)
    Signed-off-by: xil <fridalu66@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ Inspired from [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 - Preprocessing specification for additionalProperties ([#70](https://github.com/opensearch-project/opensearch-protobufs/pull/70)))
 - Preprocessing spec for changing const to boolean. ([#72](https://github.com/opensearch-project/opensearch-protobufs/pull/72))
 - Add IndexDocument, UpdateDocument, GetDocument, DeleteDocument protos and move services to protos/services folder ([#73](https://github.com/opensearch-project/opensearch-protobufs/pull/73)))
+- Preprocessing oneof by aggregating array of item and single item ([#76](https://github.com/opensearch-project/opensearch-protobufs/pull/76))
 
 ### Removed
 
--- tools/proto-convert/src/TypeModifier.ts
+++ tools/proto-convert/src/TypeModifier.ts
@@ -1,37 +1,50 @@
 import type {OpenAPIV3} from "openapi-types";
 import { traverse } from './utils/OpenApiTraverser';
+import isEqual from 'lodash.isequal';
+
+
 export class TypeModifier {
     public modify(OpenApiSpec: OpenAPIV3.Document): any {
         traverse(OpenApiSpec, {
-            // change additionalProperties: true or additionalProperties:object to object
             onSchemaProperty: (schema) => {
-                if (schema.additionalProperties === true || ( typeof schema.additionalProperties === 'object' &&  this.isEmptyObjectSchema(schema.additionalProperties as OpenAPIV3.SchemaObject))) {
-                    schema.type = 'object';
-                    delete schema.additionalProperties;
-                }
+                this.handleAdditionalPropertiesUndefined(schema)
             },
             onSchema: (schema, schemaName) => {
                 if (!schema || this.isReferenceObject(schema)) return;
-                // chang const to boolean under oneof.
-                // title: schemaName + const
-                if (schema.oneOf) {
-                    for (const item of schema.oneOf) {
-                        if (item && !('$ref' in item) && item.type === 'string' && 'const' in item) {
-                            item.type = 'boolean';
-                            item.title = `${schemaName}_${item.const}`;
-                            delete item.const;
-                        }
-                    }
-                }
+                this.handleOneOfConst(schema, schemaName)
+                this.handleOneOfSimplification(schema)
+                this.handleAdditionalPropertiesUndefined(schema)
             }
         });
         return OpenApiSpec;
+    }
+
+    // Converts `additionalProperties: true` or `additionalProperties: {}` to `type: object`.
+    // Example: { additionalProperties: true } -> { type: 'object' }
+    handleAdditionalPropertiesUndefined(schema: OpenAPIV3.SchemaObject): void {
+        if (schema.additionalProperties === true || ( typeof schema.additionalProperties === 'object' &&  this.isEmptyObjectSchema(schema.additionalProperties as OpenAPIV3.SchemaObject))) {
+            schema.type = 'object';
+            delete schema.additionalProperties;
+        }
     }
 
     isReferenceObject(schema: any): schema is OpenAPIV3.ReferenceObject {
         return schema !== null && typeof schema === 'object' && '$ref' in schema;
     }
 
+    // Converts `oneOf` schemas with `const` values to boolean types.
+    // Example: oneof: [ {type: 'string', const: 'a'} ] to type: 'boolean' with title: 'schemaName_a'
+    handleOneOfConst(schema: OpenAPIV3.SchemaObject, schemaName: string): void {
+        if (schema.oneOf) {
+            for (const item of schema.oneOf) {
+                if (item && !('$ref' in item) && item.type === 'string' && 'const' in item) {
+                    item.type = 'boolean';
+                    item.title = `${schemaName}_${item.const}`;
+                    delete item.const;
+                }
+            }
+        }
+    }
     isEmptyObjectSchema(schema: OpenAPIV3.SchemaObject): boolean {
         return (
             schema.type === 'object' &&
@@ -40,6 +53,46 @@ export class TypeModifier {
             !schema.anyOf &&
             !schema.oneOf &&
             !('$ref' in schema)
+        );
+    }
+
+    // Simplify schemas with `oneOf` by aggregating items.
+    // If there are only two `oneOf` items and one matches an array schema, remove oneOf type and set type to array.
+    // If there are more than two `oneOf` items and one matches an array schema, remove that item from `oneOf`.
+    handleOneOfSimplification(schema: OpenAPIV3.SchemaObject): void{
+        if (!('$ref' in schema) && Array.isArray(schema.oneOf)) {
+            const oneOfs = schema.oneOf;
+
+            const arraySet = new Set<string>();
+            var deleteIndx = -1;
+
+            for (const oneOf of oneOfs) {
+                if (this.isArraySchemaObject(oneOf)) {
+                    arraySet.add(JSON.stringify(oneOf.items))
+                }
+            }
+            for (const oneOf of oneOfs) {
+                const oneOfStr = JSON.stringify(oneOf);
+                if (arraySet.has(oneOfStr)) {
+                    if(oneOfs.length == 2) {
+                        schema.type = 'array';
+                        (schema as OpenAPIV3.ArraySchemaObject).items = oneOf
+                        delete schema.oneOf;
+                    } else if (oneOfs.length > 2) {
+                        deleteIndx = oneOfs.findIndex(item => isEqual(item, oneOf));
+                        oneOfs.splice(deleteIndx, 1);
+                    }
+                }
+            }
+        }
+    }
+
+    isArraySchemaObject(schema: any): schema is OpenAPIV3.ArraySchemaObject {
+        return (
+            typeof schema === 'object' &&
+            schema !== null &&
+            schema.type === 'array' &&
+            'items' in schema
         );
     }
 }
