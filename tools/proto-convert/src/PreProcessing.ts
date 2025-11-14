364cad74dea4b8c7a3f56df8b6ff986ac0cc99df
Xi Lu <fridalu66@gmail.com>
2025-11-13 21:00:35 -0800

    Preprocessing - Consolidate global parameters into GlobalParams schema (#295)
    * Preprocessing - Consolidate global parameters into GlobalParams schema
    
    Signed-off-by: xil <fridalu66@gmail.com>
    
    * update CHANGELOG
    
    Signed-off-by: xil <fridalu66@gmail.com>
    
    ---------
    
    Signed-off-by: xil <fridalu66@gmail.com>

--- tools/proto-convert/src/PreProcessing.ts
+++ tools/proto-convert/src/PreProcessing.ts
@@ -6,6 +6,7 @@ import Logger from './utils/logger';
 import * as path from 'path';
 import {SchemaModifier} from "./SchemaModifier";
 import {VendorExtensionProcessor} from "./VendorExtensionProcessor";
+import {GlobalParameterConsolidator} from "./GlobalParamWrapper";
 import type {OpenAPIV3} from "openapi-types";
 
 let config_filtered_path: string[] | undefined;
@@ -49,7 +50,8 @@ try {
   const original_spec = read_yaml(opts.input)
   const filtered_spec = new Filter().filter_spec(original_spec, opts.filtered_path);
   const sanitized_spec = new Sanitizer().sanitize(filtered_spec);
-  const vendor_processed_spec = new VendorExtensionProcessor(sanitized_spec, logger).process();
+  const consolidated_spec = new GlobalParameterConsolidator(sanitized_spec).consolidate();
+  const vendor_processed_spec = new VendorExtensionProcessor(consolidated_spec, logger).process();
   const schema_modified_spec = new SchemaModifier(vendor_processed_spec, logger).modify();
   write_yaml(opts.output, schema_modified_spec);
 
