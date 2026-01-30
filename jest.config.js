68625e566ae6771869164c0daa925948885be31e
xil <fridalu66@gmail.com>
2026-01-30 20:12:41 +0000

    Test Coverage Improvements
    Signed-off-by: xil <fridalu66@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,9 @@ module.exports = {
   moduleDirectories: ['node_modules', './tools/proto-convert/src'],
   collectCoverageFrom: [
     'tools/proto-convert/src/**/*.ts',
-    '!tools/proto-convert/src/**/*.d.ts'
+    '!tools/proto-convert/src/**/*.d.ts',
+    '!tools/proto-convert/src/PreProcessing.ts',
+    '!tools/proto-convert/src/postprocessing/types.ts'
   ],
   testMatch: ['**/test/**/*.test.ts']
 }
